5e1c3fb72bb3ab2ecad15af9a6474d308aea0d02
manucobianco <manucobianco@gmail.com>
2017-04-05 18:28:45 -0300

    Segunda entrega
    Se midio con mas datos

--- ArrayOrdenado/src/Amain.java
+++ ArrayOrdenado/src/Amain.java
@@ -4,7 +4,7 @@ public class Amain {
 	
 		long startTime;
 		long totalTime;
-		int cantElementos = 100000;
+		int cantElementos = 1000;
 		
 		Arreglo superarray = new Arreglo(cantElementos);
 		
@@ -16,6 +16,9 @@ public class Amain {
 		 //superarray.merge();	//como era de esperar use el mergesort con 10 millones de elementos y el totalTime no supero los 2000 milisegundos.
 		 superarray.burbujeo(); //mientras que en el caso del burbujeo ya con 100.000 elementos se demoraba 17000 milisegundos en ordenar.
 		totalTime = System.currentTimeMillis() - startTime;
+		
+		//con valores mas chicos como 1000 o 10000 el merge sigue siendo mas rapido, aunque la diferencia no es mucha al no haber muchos datos.
+		//a medida que aumento los datos, el merge no cambia mucho, mientras que el el burbujeo con gran volumen de datos es muy poco eficas. 
 		
 		
 		System.out.println("Tiempo total: "+totalTime);
